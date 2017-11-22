52f5854b88643becc065aefd9a67359a6573d749
Johnson Chao <chao.hs1031@gmail.com>
2017-11-22 17:19:55 +0800

    Hotfix.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # Compile yacc and lex
 yacc -d -v parser.y
 lex scanner.l
-gcc y.tab.c lex.yy.c -ly -ll -o parser
+gcc y.tab.c lex.yy.c -ly -ll -o parser.out
 
 # Remove useless files
 rm y.tab.*
@@ -9,18 +9,18 @@ rm y.output
 rm lex.yy.c
 
 # Testing
-echo "\n\nStart testing."
+printf "\n\nStart testing.\n"
 TEST_FILE="test.p"
-./parser TestCase/Project2/$TEST_FILE >> ans.log 2>&1
+./parser.out TestCase/Project2/$TEST_FILE >> ans.log 2>&1
 
 DIFF_RESULT=$(diff ans.log TestCase/Project2/ExAns/r$TEST_FILE)
 if [ "$DIFF_RESULT" = "" ]; then
-    echo "Your answer is correct!"
+    printf "Your answer is correct!\n"
 else
-    echo "Your answer is different!"
-    echo "\nYour answer:"
+    printf "Your answer is different!\n"
+    printf "\nYour answer:\n"
     cat ans.log
-    echo "\nExample answer:"
+    printf "\nExample answer:\n"
     cat TestCase/Project2/ExAns/r$TEST_FILE
 fi
 rm ans.log
