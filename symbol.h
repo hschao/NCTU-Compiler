31bf62ad001901ca2d50423d6723c8b8ba6a7cb7
Johnson Chao <chao.hs1031@gmail.com>
2017-12-03 03:54:20 +0800

    Update data structure.

--- symbol.h
+++ symbol.h
@@ -1,12 +1,39 @@
+typedef struct _ListNode {
+  void* data;
+  struct _ListNode* next;
+} ListNode;
 
+typedef enum _Kind {
+  PROG, FUNC, PARAM, VAR, CONSTANT
+} Kind;
+
+typedef enum _TypeName {
+  INTEGER, REAL, BOOLEAN, STRING, ARRAY;
+} TypeName;
+
+typedef struct _ArrayInfo {
+  TypeName typeName;
+  ListNode* dimLst;
+} ArrayInfo;
 
 typedef struct _Type {
-  unsigned kind;
-  struct _Type* array_type;
+  TypeName typeName;
+  ArrayInfo arrayInfo;
 } Type;
+
+typedef union _Attr {
+  union constant {
+    int integer;
+    float real;
+    bool bl;
+    char* str; 
+  }, 
+  ListNode* paramLst;
+} Attr;
 
 typedef struct _SymbolTableEntry {
   char name[35];
+  Kind kind;
   Type type;
   struct _SymbolTableEntry* next;
 } SymbolTableEntry;
